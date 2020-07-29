235c39f8b2e237c05afcf293ba70f2e625894dbb
Pablo Villalba <garanaw@gmail.com>
2020-07-29 18:40:54 +0100

    Added navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,10 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>{{ config('app.name') }}</title>
+        
+        <link rel="icon" type="image/png" href="{{ asset('storage/' . config('app.favicon')) }}">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -70,6 +74,32 @@
     
     <body>
         <div id="app">
+            
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('storage/small-world-money-transfer-blue.png') }}" alt="brand logo">
+                </a>
+                
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="{{ route('welcome') }}">
+                                Home <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('currency.create') }}">
+                                {{ __('currency.create') }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                
+            </nav>
+            
             <main class="pt-4 container-fluid">
                 @yield('content')
             </main>
