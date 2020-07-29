068c7446727dd358bb7b1fbf7cba7f3268b8ca85
Pablo Villalba <garanaw@gmail.com>
2020-07-29 18:06:22 +0100

    Modified welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ config('app.name') }}
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ route('currency.create') }}">Add a new currency</a>
+                    <a href="https://github.com/Garanaw/smallworld" target="_blank">
+                        View in GitHub
+                    </a>
+                    <a href="https://github.com/Garanaw">View my github</a>
                 </div>
             </div>
         </div>
